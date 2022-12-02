550c3009b53b8a5830ece24ab2cd076ec447ca6a
jeffreylu0 <jljefflu@gmail.com>
2022-12-02 08:41:13 -0500

    added EC2 setup script and updated .dockerignore

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+image="jeffreylu0/summarizer"
+user="ai-txg-1-user"
+
+sudo yum update
+sudo yum install docker
+
+# Add group membership for user to run docker commands without sudo
+sudo usermod -a -G docker $user && newgrp docker
+
+sudo systemctl enable docker.service # enable docker service at boot time
+sudo systemctl start docker.service # start docker service
+
+docker pull $image
+docker run -dp 80:80 $image
